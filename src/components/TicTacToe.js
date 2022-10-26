6fc2b0fa8172926d235134edc577a57cede91528
SEVGI1231 <sevgi2308@gmail.com>
2022-10-26 12:55:42 +0300

    isWin function done, working two steps late

--- src/components/TicTacToe.js
+++ src/components/TicTacToe.js
@@ -1,19 +1,50 @@
 import styles from "./tictactoe.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 export function TicTacToe() {
   const [cells, selCells] = useState({});
   const [turn, setTurn] = useState("X");
-
+  const [winner, setWinner] = useState(false);
+  function isWin(cells, turn) {
+    let count = 0;
+    const cases = [
+      [1, 2, 3],
+      [4, 5, 6],
+      [7, 8, 9],
+      [1, 4, 7],
+      [2, 5, 8],
+      [3, 6, 9],
+      [1, 5, 9],
+      [3, 5, 7],
+    ];
+    for (let item of cases) {
+      let count = 0;
+      item.map((num) => {
+        if (cells[num] === turn) {
+          count += 1;
+        }
+      });
+      if (count === 3) {
+        alert(`${turn} is the winner!`);
+        return true;
+      }
+    }
+  }
   function handleClick(num) {
     if (turn === "X") {
       setTurn("O");
     } else {
       setTurn("X");
     }
-    selCells({ ...cells, [num]: turn });
     console.log("Clicked!", num);
-    console.log("Cells:", cells);
+    selCells({ ...cells, [num]: turn });
+    if (isWin(cells, turn)) {
+      setWinner(turn);
+    }
   }
+  console.log(isWin(cells, turn));
+  console.log("Cells:", cells);
+  console.log("turn:", turn);
+  console.log("winner:", winner);
   const Cell = ({ num }) => {
     return <td onClick={() => handleClick(num)}>{cells[num]}</td>;
   };
